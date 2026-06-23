0e9cf24e7ffd5bbd0eaff363741a41d27c48133e
Thewasdguy <dearitra27@gmail.com>
2026-06-23 21:32:56 +0530

    a lil more2x

--- Dictionary/fibonacci dictionery map.py
+++ Dictionary/fibonacci dictionery map.py
@@ -1,23 +1,19 @@
-fib_cache = {0: 0, 1: 1}
+fib = {0: 0, 1: 1}
 
 def get_fibonacci(n):
-    if n in fib_cache:
-        return fib_cache[n]
+    if n in fib:
+        return fib[n]
     
-    fib_cache[n] = get_fibonacci(n - 1) + get_fibonacci(n - 2)
-    return fib_cache[n]
+    fib[n] = get_fibonacci(n - 1) + get_fibonacci(n - 2)
+    return fib[n]
 
-try:
-    count = int(input("How many Fibonacci numbers do you want to generate?: "))
+count = int(input("How many Fibonacci numbers do you want to generate?: "))
+
+if count <= 0:
+    print("Enter a number greater than 0.")
+else:
+    get_fibonacci(count - 1)
     
-    if count <= 0:
-        print("Enter a number greater than 0.")
-    else:
-        get_fibonacci(count - 1)
-        
-        print("Here is your Fibonacci numbers:")
-        for position in range(count):
-            print(f"{position} -> {fib_cache[position]}")
-
-except ValueError:
-    print("Please enter a valid whole number.")
+    print("Here is your Fibonacci numbers:")
+    for position in range(count):
+        print(f"{position} -> {fib[position]}")
